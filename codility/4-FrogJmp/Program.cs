10aebf2d49d5effd3a8dc8ae2b4bd3e9a043762c
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2020-11-17 00:12:34 +0100

    solution of FrogJmp

---
+++ codility/4-FrogJmp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _4_FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.solution(5, 105, 3));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int X, int Y, int D)
+        {
+            var distance = Math.Abs(Y - X);
+            return distance % D > 0 ? distance / D + 1 : distance / D;
+        }
+    }
+}
