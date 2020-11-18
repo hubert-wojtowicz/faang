4adafefe438b4b0a11ef86c61b9cf08928ae83c4
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2020-11-18 08:35:29 +0100

    removed comments from template

--- codility/template.cs
+++ codility/template.cs
@@ -27,20 +27,7 @@ namespace LXTY_NameOfTask
     }
 
     /*
-     * Path to walk 0,1,...,X,X+1,  
-     * 1,...,X positions inside of river
-     * A:a0,...,a(N-1) 
-     * ai - falling leaves position in i time 
-     * 
-     * 1) this task is question if leaves exists at every position 1,2,...X, and what is the earliest time to get on the other side
-     * 2) there can be no solution
-     * 3) what we can do is to count leaves at every position from 1,...,X but once (bit array 0,1 where 0 no leaves, and 1 at least one leaves)
-     * 4) accumulate continously sum of bits
-     * 5) state frog can pass river as bit sum exqual toriver witdth
-     * All of those can beacheived at O(N) time
-     * N is max 100 000 so:
-     * - int array size is 400 000byte = 390KB
-     * - bool array size is 100 000 byte = 93KB
+     * This is place for analisys
      */
 
     class Solution
