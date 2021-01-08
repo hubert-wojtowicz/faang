508289a4d454e3cee4c7b0c900b01ee0789f66c9
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2021-01-08 23:33:38 +0100

    solved max water container problem

---
+++ udemy-faang-course/2-container with most water/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _2_container_with_most_water
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var sol = new Solution();
+
+			Console.WriteLine(sol.MaxArea(new int[]{1,6,9,3,4,5,8}));
+        }
+	
+    }
+
+	public class Solution
+	{
+		public int MaxArea(int[] height)
+		{
+			int n = height.Length;
+			int a = 0;
+			int b = n > 0 ? n - 1 : 0;
+			int maxArea = 0;
+
+			while (a != b)
+			{
+				maxArea = Math.Max(maxArea, Math.Min(height[a], height[b]) * (b - a));
+
+				if (height[a] > height[b])
+				{
+					--b;
+				}
+				else
+				{
+					++a;
+				}
+			}
+
+			return maxArea;
+		}
+	}
+}
