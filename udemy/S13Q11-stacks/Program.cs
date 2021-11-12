a1594962033300cd50bf4e2334dced24ce817f4f
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2021-11-12 11:18:30 +0100

    Bracket problem

---
+++ udemy/S13Q11-stacks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace S13Q11_stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($" \"\" - {CountBracketsToRemove("")}");
+            Console.WriteLine($" \"()\" - {CountBracketsToRemove("()")}");
+            Console.WriteLine($" \"(\" - {CountBracketsToRemove("(")}");
+            Console.WriteLine($" \")\" - {CountBracketsToRemove(")")}");
+            Console.WriteLine($" \"(()())()\" - {CountBracketsToRemove("(()())()")}");
+            Console.WriteLine($" \"((\" - {CountBracketsToRemove("((")}");
+            Console.WriteLine($" \"))\" - {CountBracketsToRemove("))")}");
+            Console.WriteLine($" \"()(()))()())\" - {CountBracketsToRemove("()(()))()())")}");
+        }
+
+        static int CountBracketsToRemove(string s)
+        {
+            int opening = 0;
+            int closingToRemove = 0;
+
+            for(int i=0;i<s.Length;i++)
+            {
+                if(s[i]=='(')
+                    opening++;
+
+                if(s[i]==')')
+                {
+                    if(opening>0)
+                        opening--;
+                    else
+                        closingToRemove++;
+                }
+            }
+            return opening + closingToRemove;
+        }
+    }
+}
