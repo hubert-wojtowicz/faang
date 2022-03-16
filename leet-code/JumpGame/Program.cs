bebda27df3bea8fba0c4bf59594d8b9f74fde266
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2022-03-16 20:45:23 +0100

    another solutions

---
+++ leet-code/JumpGame/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+var sol = new Solution();
+Console.WriteLine(sol.CanJump(new[] { 3, 2, 1, 0, 4 }));
+Console.WriteLine(sol.CanJump(new[] { 2, 3, 1, 1, 4 }));
+
+
+public class Solution
+{
+    public bool CanJump(int[] nums)
+    {
+        BitArray goodIdxs = new BitArray(nums.Length);
+        goodIdxs[nums.Length - 1] = true;
+
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            var jumpRangeWithinArray = nums.Length - (i + 1);
+            var jump = Math.Min(nums[i], jumpRangeWithinArray);
+            while (jump > 0)
+            {
+                if (goodIdxs[i + jump])
+                {
+                    goodIdxs[i] = true;
+                    break;
+                }
+                jump--;
+            }
+        }
+
+        return goodIdxs[0];
+    }
+}
---
+++ leet-code/LongestSubstringwithAtMostTwoDistinctCharacters/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+var sol = new Solution();
+
+Console.WriteLine(sol.LengthOfLongestSubstringTwoDistinct("ababcbcbaaabbdef") == 6);
+Console.WriteLine(sol.LengthOfLongestSubstringTwoDistinct("abccbbcccaaacaca") == 10);
+Console.WriteLine(sol.LengthOfLongestSubstringTwoDistinct("ccaabbb") == 5);
+Console.WriteLine(sol.LengthOfLongestSubstringTwoDistinct("eceba") == 3);
+Console.WriteLine(sol.LengthOfLongestSubstringTwoDistinct("aa") == 2);
+Console.WriteLine(sol.LengthOfLongestSubstringTwoDistinct("a") == 1);
+public class Solution
+{
+    public int LengthOfLongestSubstringTwoDistinct(string s)
+    {
+        Dictionary<char, int> lastChangeTwoChars = new Dictionary<char, int>();
+        int l = 0, r = 0;
+        lastChangeTwoChars.Add(s[l], l);
+        while (r < s.Length && lastChangeTwoChars.ContainsKey(s[r])) r++;
+        if (r >= s.Length) return s.Length;
+        lastChangeTwoChars.Add(s[r], r);
+        int maxSeen = r - l + 1;
+        r += 1;
+        while (l < r && r < s.Length)
+        {
+            if (lastChangeTwoChars.ContainsKey(s[r]))
+            {
+                if (s[r - 1] != s[r])
+                    lastChangeTwoChars[s[r]] = r;
+                maxSeen = Math.Max(maxSeen, r - l + 1);
+            }
+            else
+            {
+                var newl = lastChangeTwoChars[s[r - 1]];
+                var toRemove = lastChangeTwoChars.First(x => x.Key != s[r - 1]);
+                lastChangeTwoChars.Remove(toRemove.Key);
+                l = newl;
+                lastChangeTwoChars.Add(s[r], r);
+            }
+            r++;
+        }
+
+        return maxSeen;
+    }
+}
